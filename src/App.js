a58f2b1c5600c4c375db368a9312807a7dd89e96
khushiisinha <khushiisinha01@gmail.com>
2024-01-15 14:45:46 +0530

    HashRouter commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 
