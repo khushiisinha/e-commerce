5b807b73b49d98664e585caa30112c13562e1497
khushiisinha <khushiisinha01@gmail.com>
2024-01-15 14:17:40 +0530

    new commit app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { HashRouter , Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <div className="">
-    <HashRouter>
-    <Routes>
-        <Route path="/e-commerce" element={<LoginPage />} />
-        <Route path="/e-commerce/login" element={<LoginPage/>} />
-        <Route path="/e-commerce/dashboard" element={<Dashboard/>} />
-    </Routes>
-</HashRouter>
-  </div>;
+  return (
+    <div className="">
+      <Router basename="/e-commerce">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
